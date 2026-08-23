c9cc1289c9fd435bbb0504f74538bd6202d1831c
Priyanshu Kumar <poddarpriyanshu308@gmail.com>
2026-08-23 18:34:34 +0530

    Checking prime numbers

--- 009_check_primenumber.cpp
+++ 009_check_primenumber.cpp
@@ -1,22 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
-
-	int n;
-	cin >> n;
-
-	int i = 2;
-	while (i <= n - 1) {
-		if (n % i == 0) {
-			cout << "not prime" << endl;
-			break;
-		}
-		i = i + 1;
-	}
-	if (i == n) {
-		cout << "prime" << endl;
-	}
     
-	return 0;
+    int n;
+    cin >> n;
+
+    bool ans = true;
+
+    for (int i = 2; i < n; i++)
+    {
+ 			if (n % i == 0)
+ 			{
+ 				ans = false;
+ 			}
+    }
+    if (ans)
+    {
+    	cout << "True";
+    }else{
+    	cout << "False";
+    }
+    return 0;
 }
