5b7cd1ded72eafb9d903ef878bb73ebb90b529a6
Priyanshu Kumar <poddarpriyanshu308@gmail.com>
2026-08-23 17:05:22 +0530

    Solution

--- 005_fibonacci_check.cpp
+++ 005_fibonacci_check.cpp
@@ -5,21 +5,37 @@ int main() {
     
     int n;
     cin >> n;
+    int target;
+    cin >> target;
 
     int first = 0;
     int second = 1;
 
     cout << first << " " << second << " ";
     int sum = 0;
+    bool ans = false;
 
     for(int count = 2; count < n; count++){
     	 sum = first + second;
     	 cout << sum << " ";
+
+         if (target == sum)
+         {
+            ans = true;
+         }
+
     	 first = second;
     	 second = sum;
     } 
 
     cout << endl;
+
+    if(ans){
+        cout << "True" << endl;
+    }else{
+        cout << "False" << endl;
+
+    }
     
     return 0;
 }
