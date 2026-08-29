ccfb3921e46df7ba13fdd865d9c63566ad7dc073
Priyanshu Kumar <poddarpriyanshu308@gmail.com>
2026-08-29 12:21:11 +0530

    optimised bubble sort

---
+++ 017_bubble_sort_optimized.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        // If no two elements were swapped in the inner loop, then break
+        if (!swapped)
+            break;
+    }
+}
