b27e8e1a76336d40d7666ce8ba958d7a63f2a881
Priyanshu Kumar <poddarpriyanshu308@gmail.com>
2026-08-29 12:19:36 +0530

    basic paas by reference

---
+++ 018_pass_by_reference.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b){
+	int c = a + b;
+		return c;
+}
+
+int main() {
+    // your code here
+
+    int x = add(3, 5);
+    cout << x;
+    
+    return 0;
+}
