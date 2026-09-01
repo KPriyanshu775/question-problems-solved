c37d4f3e081402dda5564c3259a161d7c6c0d57f
Priyanshu Kumar <poddarpriyanshu308@gmail.com>
2026-09-01 12:35:26 +0530

    selection sort

---
+++ 025_selection_sort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int arr[] = {5,8,2,4,6,1};
+    int n = sizeof(arr) / sizeof(int);
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		int min = i;
+		for (int j = i + 1; j < n; j++)
+		{
+			if(arr[j] < arr[min]){
+				min = j;
+			}
+		}
+
+		swap(arr[i], arr[min]);
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+
+    return 0;
+}
