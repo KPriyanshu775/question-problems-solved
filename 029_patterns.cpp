25e33bdb847e0a948207795d837c858b6c58a5d0
Priyanshu Kumar <poddarpriyanshu308@gmail.com>
2026-09-09 14:15:11 +0530

    updated new pattern

--- 029_patterns.cpp
+++ 029_patterns.cpp
@@ -27,6 +27,26 @@
 //     }
 // }
 
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n = 5;
+
+//     for(int i = 1; i <= n; i++){
+//         for(int j = 1; j <= n - i; j++){
+//             cout << "  ";
+//         }
+//         for(int k = 1; k <= i; k++){
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
+
+
 #include <iostream>
 using namespace std;
 
@@ -37,7 +57,7 @@ int main() {
         for(int j = 1; j <= n - i; j++){
             cout << "  ";
         }
-        for(int k = 1; k <= i; k++){
+        for(int k = 1; k <= (2 * i - 1); k++){
             cout << "* ";
         }
         cout << endl;
@@ -45,4 +65,3 @@ int main() {
 
     return 0;
 }
-
