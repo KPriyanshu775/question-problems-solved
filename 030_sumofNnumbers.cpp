33aad4f33102046f3a6a29f04a5e6f9108d93618
Priyanshu Kumar <poddarpriyanshu308@gmail.com>
2026-09-11 00:08:20 +0530

    sum of n numbers

---
+++ 030_sumofNnumbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    int high = 5;
+   int sum = 0;
+    for(int low = 1; low <= high; low++){
+        sum = sum + low;
+       
+   } 
+   cout << sum;
+   
+    return 0;
+}   
