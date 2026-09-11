ed78e722947863b73749bc6fd80f90ba43956b69
Priyanshu Kumar <poddarpriyanshu308@gmail.com>
2026-09-11 13:58:35 +0530

    made changes

--- 030_sumofNnumbers.cpp
+++ 030_sumofNnumbers.cpp
@@ -1,3 +1,19 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+   
+//     int high = 5;
+//    int sum = 0;
+//     for(int low = 1; low <= high; low++){
+//         sum = sum + low;
+       
+//    } 
+//    cout << sum;
+   
+//     return 0;
+// }   
+
 #include <iostream>
 using namespace std;
 
