18ef3471f9ea0d9eb14ceba6395ad582a1339eea
Priyanshu Kumar <poddarpriyanshu308@gmail.com>
2026-09-18 15:04:40 +0530

    added code

---
+++ new.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+struct Item{
+    int weight;
+    int value;
+    float ratio;
+};
+
+int main() {
+    int n, capacity;
+    float totalValue = 0;
+
+    printf("Enter number of item: ");
+    scanf("%d", &n);
+
+    struct Item 
+}
